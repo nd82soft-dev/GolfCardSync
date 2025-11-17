3d3d4bad22105dfe55a1e8e1622094f54b02a63b
nd82soft-dev <nd82soft@gmail.com>
2025-11-17 14:25:12 -0700

    Delete api directory

--- api/ScoreCardReader.js
+++
@@ -1,7 +0,0 @@
-
-  return (
-    <div className="min-h-screen bg-gray-900 font-sans antialiased flex flex-col">
-      {/* Header is fixed at the top */}
-      <Header title={'GolfCardSync'} />
-
-
